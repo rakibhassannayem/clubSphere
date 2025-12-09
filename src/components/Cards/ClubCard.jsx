ba06d669b86d64f2fb031c2006193763c4f665a7
rakibhassannayem <rakibhassannayem@gmail.com>
2025-12-09 19:39:47 +0600

    fix card

--- src/components/Cards/ClubCard.jsx
+++ src/components/Cards/ClubCard.jsx
@@ -1,3 +1,4 @@
+import { FiUsers } from "react-icons/fi";
 import { IoLocationOutline } from "react-icons/io5";
 
 const ClubCard = ({ club }) => {
@@ -8,23 +9,33 @@ const ClubCard = ({ club }) => {
     location,
     bannerImage,
     membershipFee,
+    members,
   } = club;
   return (
-    <div className="card bg-base-100 shadow-sm">
-      <figure className="">
+    <div className="card bg-base-100 shadow-sm hover:shadow-xl hover:scale-102 transition cursor-pointer">
+      <figure className="relative">
         <img
           src={bannerImage}
           alt={clubName}
-          className="h-48 w-full object-cover"
+          className="h-42 w-full object-cover"
         />
-        <span className="badge bg-base-200 text-black absolute top-3 right-3 font-bold">
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-black/30"></div>
+
+        <span className="badge bg-base-300 text-black absolute top-3 right-3 font-bold">
           {membershipFee == 0 ? "free" : <span>${membershipFee}/mo</span>}
         </span>
+
+        <div className="text-white flex items-center gap-1 absolute bottom-3 right-3">
+          <FiUsers />
+          {members}
+        </div>
       </figure>
       <div className="p-3">
-        <span className="badge bg-primary/10 text-primary">{category}</span>
-        <h2 className="card-title">{clubName}</h2>
-        <p className="text-accent">{description}</p>
+        <span className="badge bg-primary/10 text-primary font-medium">{category}</span>
+        <h2 className="card-title mt-2">{clubName}</h2>
+        <p className="text-accent my-2">{description}</p>
         <p className="text-accent flex items-center gap-1">
           <IoLocationOutline />
           {location}
